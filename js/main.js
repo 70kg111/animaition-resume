897b31e41772735a484980fbb4f04a1cf25a1bb6
70kg111 <76104718@qq.com>
2019-05-05 23:39:37 +0800

    change time 2

--- js/main.js
+++ js/main.js
@@ -133,7 +133,7 @@ function wirteCode(prefix, code, fn) {
             window.clearInterval(id)
             fn && fn.call()   //这里一定要调用一下
         }
-    }, 0)
+    }, 50)
 }
 
 
@@ -163,5 +163,5 @@ function writeMarkdown(markdown, fn) {
             window.clearInterval(id)
             fn && fn.call()   //这里一定要调用一下
         }
-    }, 100)
+    }, 50)
 }
